7e4061aa739bad3febdf0f56349230d9b68a36a4
Liegia <k.heidenborg@gmail.com>
2023-12-27 22:36:01 +0100

    nvim 2024

---
+++ nvim/lua/heiden/init.lua
@@ -0,0 +1,3 @@
+require("heiden.remap") -- source remap every time nvim is loaded
+require("heiden.set")
+print("fckem") -- remind your self every time nvim opens
---
+++ nvim/lua/heiden/remap.lua
@@ -0,0 +1,20 @@
+vim.g.mapleader = " " --set space to leader
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- in normal mode ("n"), space + p + v, exc :Ex
+
+vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("v", "jk", "<Esc>")
+
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gb") -- in v mode allows to shift the line up
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gb")
+
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz") -- C-d down half a page but the cursor stays 
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
+vim.keymap.set("n", "n", "nzzzv") -- Keeps the search in the middle of the screen
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "<leader>y", "\"+y") -- leader y now copies to system clipboard
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>y", "\"+y")
